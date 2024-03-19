32bf4f9e9ebfcf0052940484163e18a4c34819d4
chienhuak <christy.kch.c@gmail.com>
2024-03-19 14:14:06 +0800

    adjust application h5 to show / hide button

---
+++ application/arrowscript.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function myFunction() {
+    var x = document.getElementById("myDIV");
+    var icon = document.getElementById("toggleIcon");
+    
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      icon.innerText = " ▲(Click to hide)"; // 顯示時切換為 Hide icon
+    } else {
+      x.style.display = "none";
+      icon.innerText = " ▼(Click to expand)"; // 隱藏時切換為 Expand icon
+    }
+  }
