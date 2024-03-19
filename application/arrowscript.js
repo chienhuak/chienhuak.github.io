1d2046241e7ce79e8fe54a58b80f5864b2c013a4
chienhuak <christy.kch.c@gmail.com>
2024-03-19 14:29:14 +0800

    debug : adjust show / hide button script path

--- application/arrowscript.js
+++ application/arrowscript.js
@@ -7,6 +7,34 @@ function myFunction() {
     }
   }
   
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    var icon = document.getElementById("toggleIcon");
+    
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      icon.innerText = " ▲(Click to hide)"; // 顯示時切換為 Hide icon
+    } else {
+      x.style.display = "none";
+      icon.innerText = " ▼(Click to expand)"; // 隱藏時切換為 Expand icon
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   function myFunction() {
     var x = document.getElementById("myDIV");
     var icon = document.getElementById("toggleIcon");
