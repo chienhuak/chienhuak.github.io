e3793d70cb18646e01eb00d16de1d69209673a45
chienhuak <christy.kch.c@gmail.com>
2024-04-29 02:19:55 +0800

    week4 : task 4-3 (add sign-out endpoint)

--- week4/main.py
+++ week4/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI, Form, Request, Response, status
+from fastapi import FastAPI, Form, Request, Response, HTTPException, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from typing import Optional
 from typing import Annotated
+from starlette.applications import Starlette
+from starlette.middleware import Middleware
+from starlette.middleware.sessions import SessionMiddleware
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -17,22 +22,30 @@ async def root(request:Request):
     return templates.TemplateResponse("index.html",{"request":request})
 
 
+@app.get("/TEST",response_class=HTMLResponse)
+async def root(request:Request):
+    return templates.TemplateResponse("test.html",{"request":request})
+
+
+
 @app.post("/signin",response_class=HTMLResponse)
 #async def signin(username: Annotated[str, Form()], password: Annotated[str, Form()]):
-async def signin(username: str = Form(...), password: str = Form(...), agree: bool = Form(...)):
+#async def signin(username: str = Form(...), password: str = Form(...), agree: bool = Form(...)):
+async def signin(request: Request, username: Optional[str] = Form(None), password: Optional[str] = Form(None), agree: bool = Form(...)):
     # 使用從表單中接收到的數據進行後續處理
-    print("xxxxxx")
-    print("Received username:", username)
-    print("Received password:", password)
+    # print("Received username:", username)
+    # print("Received password:", password)
     if username == "admin" and password == "password" and agree == True:
         #登入成功，導向到會員頁面
         return RedirectResponse(url="/member")
         # return templates.TemplateResponse("member.html",{"request":Request})
+    if username is None or password is None:
+        error_msg = "缺少帳號或密碼"
+        return RedirectResponse(url=f"/error?msg={error_msg}")
     else:
         # 登入失敗，返回登入頁面
-        print("bbbbbb")
-        return RedirectResponse(url="/error")
-        # return templates.TemplateResponse("index.html", {"request": Request, "msg": "登入失敗，請檢查帳號、密碼和同意條款"})
+        error_msg = "驗證失敗"
+        return RedirectResponse(url=f"/error?msg={error_msg}")
 
 
 @app.post("/member", response_class=HTMLResponse)
@@ -40,9 +53,19 @@ async def member(request: Request):
     return templates.TemplateResponse("member.html", {"request": request})
 
 
-@app.post("/error/{msgnote}", response_class=HTMLResponse)
+@app.post("/error", response_class=HTMLResponse)
 async def member(request: Request):
     return templates.TemplateResponse("retry.html", {"request": request})
+
+
+@app.post("/error", response_class=HTMLResponse)
+async def error(request: Request, msg: str = None):
+    return templates.TemplateResponse("retry.html", {"request": request, "msg": msg})
+
+
+@app.get("/signout",response_class=HTMLResponse)
+async def signout(request:Request):
+    return RedirectResponse(url="/")
 
 
 
