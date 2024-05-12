3ef89e40ff6e49c443378a538e10a0256b2d5682
chienhuak <christy.kch.c@gmail.com>
2024-05-12 11:05:23 +0800

    week6 : (add ul if it's reply messages)

--- week6/main.py
+++ week6/main.py
@@ -91,43 +91,6 @@ async def signup(request: Request, name: Optional[str] = Form(None), username0: 
         return RedirectResponse(url="/error?msg=user_not_exist", status_code=status.HTTP_303_SEE_OTHER)
 
 
-# @app.post("/signin",response_class=HTMLResponse)
-# #async def signin(username: Annotated[str, Form()], password: Annotated[str, Form()]):
-# #async def signin(username: str = Form(...), password: str = Form(...), agree: bool = Form(...)):
-# async def signin(request: Request, username: Optional[str] = Form(None), password: Optional[str] = Form(None), agree: bool = Form(...)):
-#     # 使用從表單中接收到的數據進行後續處理
-#     # print("Received username:", username)
-#     # print("Received password:", password)
-#     if username == "test" and password == "test" and agree == True:
-#         # 登入成功，將使用者狀態設置為已登入
-#         request.session['SIGNED-IN'] = True
-#         #登入成功，導向到會員頁面
-#         #如果沒有提供 status_code 參數，則 FastAPI 將使用默認的狀態碼，但這可能不是你想要的。因此，你需要明確指定。 
-#         return RedirectResponse(url="/member", status_code=status.HTTP_303_SEE_OTHER)
-#         # return templates.TemplateResponse("member.html",{"request":Request})
-#     if username is None or password is None:
-#         # error_msg = "缺少帳號或密碼"
-#         return RedirectResponse(url="/error?msg=no_username_or_password", status_code=status.HTTP_303_SEE_OTHER)
-#     else:
-#         # 登入失敗，返回登入頁面
-#         # error_msg = "驗證失敗"
-#         # return RedirectResponse(url=f"/error?msg={error_msg}", status_code=status.HTTP_303_SEE_OTHER)
-#         return RedirectResponse(url="/error?msg=user_not_exist", status_code=status.HTTP_303_SEE_OTHER)
-
-
-@app.get("/math",response_class=HTMLResponse)
-async def math(request: Request, num: str):   #GET不會返回Form的值，會顯示在URL參數上面
-    if num.isdigit():
-         return RedirectResponse(url="/square/"+num)
-    else:
-        return RedirectResponse(url="/error?msg=not_positive_number", status_code=status.HTTP_303_SEE_OTHER)
-
-
-@app.get("/square/{num}", response_class=HTMLResponse)
-async def square(request: Request, num: Optional[int]=None):
-    return templates.TemplateResponse("square.html", {"request": request, "show_msg":num*num})
-
-
 @app.post("/createMessage", response_class=HTMLResponse)
 async def createMessage(request: Request, say: Optional[str] = Form(None)):
     if not say:
@@ -142,6 +105,7 @@ async def createMessage(request: Request, say: Optional[str] = Form(None)):
         mydb.commit()
 
     return RedirectResponse(url="/member", status_code=status.HTTP_303_SEE_OTHER)
+
 
 @app.get("/deleteMessage/{message_id}", response_class=HTMLResponse)
 async def deleteMessage(message_id: int):
@@ -181,15 +145,22 @@ async def member(request: Request):
             mycursor.execute(query)
             result = mycursor.fetchall()
 
-            # px=None
-            # for x in result:
-            #     if px and x['parent_id']:
-            #         x['reply']=True               
-            #     px=x
+            # 如果是回覆留言，則HTML要多一層ul
+            px=None
+            UL2=None
+            for x in result:
+                if not px:
+                    pass
+                else:
+                    if not UL2 and px['level']==x['level']:
+                        x['addUL']=True
+                        UL2=True
+                    if UL2 and px['level']!=x['level']:
+                        px['delUL']=True
+                        UL2=None
 
-            # 將 None 替換為 null
-            # result_null = [tuple('null' if val is None else val for val in row) for row in result]
-            # print(result_null)
+                px=x
+
         return templates.TemplateResponse("member.html", {"request": request, "show_name":name, "show_msgboard":result})
 
 
